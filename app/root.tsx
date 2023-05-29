7d58637c6595f4e3b5aeb5e6227454d45ffcd7b2
Helmi Satria <satriahelmi@gmail.com>
2023-05-29 23:47:56 +0700

    feat: add tailwind, google fonts, and minimum styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
+
 import {
   Links,
   LiveReload,
@@ -9,13 +9,25 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css";
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "stylesheet", href: styles },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    attributes: { crossorigin: "" },
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;500;600;700&display=swap",
+  },
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="id">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
--- app/routes/dashboard.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard 2</h1>
-    </div>
-  );
-}
